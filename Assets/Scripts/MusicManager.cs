23ab0e38d91880bf196108b6523f48441541dde8
Elias de M. Fernandes <eliasdemoraes@live.com>
2015-12-21 16:39:41 -0200

    First commit to the Glitch Garden game with some sprites, sounds and 6 initials scenes

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	#region Public Variables
+	public AudioClip[] levelMusicChangeArray;
+	#endregion
+	
+	#region Private Variables 
+	private AudioSource audioSource;
+	#endregion
+	
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+		Debug.Log ("Dont destroy on load: " + name);		
+	}
+	
+	void Start(){
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		Debug.Log("Music player loaded level " + levelMusicChangeArray[level]);
+		if(thisLevelMusic){
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	public AudioClip menuScene;
+	public AudioClip gameScene;
+	public AudioClip endScene;
+	
+	private AudioSource music;
+	
+	void Start () {
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();
+			music.clip = menuScene;
+			music.loop = true;
+			music.Play();
+		}
+		
+	}
+	void OnLevelWasLoaded(int level){
+		if (instance == this){
+			Debug.Log("Music player loaded level " + level);
+			music.Stop();
+			if(level == 0){
+				music.clip = menuScene;	
+			}
+			else if(level == 1){
+				music.clip = gameScene;	
+			}
+			else if(level == 2){
+				music.clip = endScene;	
+			}
+			music.loop = true;
+			music.Play();
+		}
+	}
+}
