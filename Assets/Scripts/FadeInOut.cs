e7345ac202d350fc3dcfd67cc08519f826c4979d
Elias de M. Fernandes <eliasdemoraes@live.com>
2015-12-22 01:01:26 -0200

    Added animations, some new sprites, fixed Start Menu, Pause Menu, Call for anothe scene and some new sounds

---
+++ Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeInOut : MonoBehaviour {
+	
+	#region Public Variables
+	public float fadeInTimeSec;
+	#endregion
+	
+	#region Private Variables
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+	#endregion
+	
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad < fadeInTimeSec){
+			float alphaChange = Time.deltaTime / fadeInTimeSec;
+			currentColor.a -= alphaChange;
+			fadePanel.color = currentColor;
+		}else{
+			gameObject.SetActive(false);
+		}
+	}
+}
